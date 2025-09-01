362d259182c7161b79bfcf88f34ea02d1212d6a6
Tylianthuah <tylian15@gmail.com>
2025-09-01 16:19:34 +0530

    Github Authentication

---
+++ src/app/(auth)/login/_components/LoginForm.tsx
@@ -0,0 +1,77 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { authClient } from "@/lib/auth-client";
+import { ArrowRight, GithubIcon, Loader } from "lucide-react";
+import { ReactElement, useTransition } from "react";
+import { toast } from "sonner";
+
+const LoginForm = () => {
+  const [isPending, gitHubTransition] = useTransition();
+
+  const handleGithubLogin = async () => {
+    gitHubTransition(async () => {
+      await authClient.signIn.social({
+        provider: "github",
+        callbackURL: "/",
+        fetchOptions: {
+          onSuccess: () => {
+            toast.success(
+              "Successfully logged in!. Redirecting to homepage..."
+            );
+          },
+          onError: (error) => {
+            toast.error(`Internal server Error`);
+          },
+        },
+      });
+    });
+  };
+
+  return (
+    <Card>
+      <CardHeader className="text-center">
+        <CardTitle className="text-xl">Welcome back!</CardTitle>
+        <CardDescription>Login using your Github or Email account.</CardDescription>
+      </CardHeader>
+      <CardContent className="flex flex-col gap-4">
+        <Button disabled={isPending} variant="outline" onClick={handleGithubLogin} className="w-full">
+          {isPending ? (
+            <>
+              <Loader  className="size-4 animate-spin"/>
+            </>
+          ) : (
+            <>
+              <GithubIcon className="size-4" />
+              Sign in with Github
+            </>
+          )}
+        </Button>
+
+        <div className="relative mt-2 text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
+          <span className="relative z-10 bg-card px-3 text-muted-foreground">
+            or continue with
+          </span>
+        </div>
+
+        <form className="grid gap-4">
+          <Label htmlFor="email">Email</Label>
+          <Input id="email" type="email" placeholder="email@example.com" />
+          <Button type="submit">
+            Continue with email <ArrowRight />
+          </Button>
+        </form>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default LoginForm;
--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,42 +1,17 @@
-import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { Form } from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { ArrowRight, GithubIcon } from "lucide-react";
-import React from "react";
+import { auth } from "@/lib/auth";
+import LoginForm from "./_components/LoginForm";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
-const LoginPage = () => {
-  return (
-    <Card>
-      <CardHeader className="text-center">
-        <CardTitle className="text-xl">Welcome back!</CardTitle>
-        <CardDescription>Login using your Github account.</CardDescription>
-      </CardHeader>
-      <CardContent className="flex flex-col gap-4">
-        <Button variant="outline" className="w-full">
-          <GithubIcon className="size-4" />
-          Sign in with Github
-        </Button>
+const LoginPage = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
 
-        <div className="relative mt-2 text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
-          <span className="relative z-10 bg-card px-3 text-muted-foreground">or continue with</span>
-        </div>
-        
-        <form className="grid gap-4">
-            <Label htmlFor="email">Email</Label>
-            <Input id="email" type="email" placeholder="email@example.com" />
-            <Button type="submit">Continue with email <ArrowRight /></Button>
-        </form>
-      </CardContent>
-    </Card>
-  );
+  if(session){
+    return redirect("/")
+  }
+  return <LoginForm />;
 };
 
 export default LoginPage;
